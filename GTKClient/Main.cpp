b4180a41ebcf30f0482fb7319bdacc3e2f0432f2
Stephen Lane-Walsh <sdl.slane@gmail.com>
2016-03-16 00:29:23 -0400

    Started playing with Glade Added Glade file that sort of looks like a chat list Updated GTK Client to load it

--- GTKClient/Main.cpp
+++ GTKClient/Main.cpp
@@ -8,8 +8,11 @@ int main(int argc, char** argv)
         Gtk::Application::create(argc, argv,
           "org.gtkmm.examples.base");
 
-      Gtk::Window window;
-      window.set_default_size(200, 200);
+      Glib::RefPtr<Gtk::Builder> builder = Gtk::Builder::create_from_file("FriendsList.glade");
+      Gtk::ApplicationWindow* pWindow = nullptr;
 
-      return app->run(window);
+      builder->get_widget("win_Main", pWindow);
+      pWindow->set_default_size(300, 600);
+
+      return app->run(*pWindow);
 }
